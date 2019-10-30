8088cda0fd4dabb6cc8428311a15699d69d39bc0
Connor Grant <connor.grant@ibm.com>
2019-10-30 14:40:21 -0500

    New functionality

---
+++ doorAnim.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimation : MonoBehavior {
+    [SerializeField] private Animator myAnimationController;
+
+    private void OnTriggerEnter (Collider other) {
+        if (other.CompareTag ("Player")) {
+            myAnimationController.SetBool ("openDoor", true);
+        }
+    }
+    private void OnTriggerExit (Collider other) {
+        if (other.CompareTag ("Player")) {
+            myAnimationController.SetBool ("openDoor", false);
+        }
+
+    }
+
+}
