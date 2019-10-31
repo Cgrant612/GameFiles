02a8ec1cae0d788b1115c6eb1bbe594b69348998
Connor Grant <connor.grant@ibm.com>
2019-10-31 08:21:11 -0500

    Updated doorAnim to include stay function

--- doorAnim.cs
+++ doorAnim.cs
@@ -2,19 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorAnimation : MonoBehavior {
+public class DoorAnimation : MonoBehavior
+{
     [SerializeField] private Animator myAnimationController;
 
-    private void OnTriggerEnter (Collider other) {
-        if (other.CompareTag ("Player")) {
-            myAnimationController.SetBool ("openDoor", true);
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            myAnimationController.SetBool("openDoor", true);
         }
     }
-    private void OnTriggerExit (Collider other) {
-        if (other.CompareTag ("Player")) {
-            myAnimationController.SetBool ("openDoor", false);
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            myAnimationController.SetBool("openDoor", false);
         }
 
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            myAnimationController.SetBool("doorIsOpen", true);
+        }
     }
 
 }
