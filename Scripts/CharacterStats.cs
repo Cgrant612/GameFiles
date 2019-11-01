492768ab8a740ff926ab4d71e15f6acac9a8e093
Connor Grant <connor.grant@ibm.com>
2019-11-01 13:47:39 -0500

    added CharacterStats script

---
+++ Scripts/CharacterStats.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    float meleeAttack;
+    float defense;
+    float stamina;
+    float accuracy;
+    float critical;
+    float magicAttack;
+    float magicDefense;
+    float luck;
+    float hpRecovery;
+    float manaRecovery;
+    float carryCapacity;
+
+}
